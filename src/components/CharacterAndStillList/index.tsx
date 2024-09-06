e03ab19fa1e885499e674e3347a59d94fa33f904
chira <chira2chira2chira2chira@gmail.com>
2024-09-07 02:48:31 +0900

    LazyLoad導入

--- src/components/CharacterAndStillList/index.tsx
+++ src/components/CharacterAndStillList/index.tsx
@@ -1,9 +1,10 @@
-import { displayCharClass } from "@/utils/stringUtil";
-import { CharInfoWithStill } from "@/utils/yamlUtil";
-import { Button, Card, Icon, Tooltip } from "@blueprintjs/core";
+import { useState } from "react";
+import LazyLoad from "react-lazyload";
 import { css } from "@emotion/react";
 import { useTranslation } from "next-i18next";
-import { useState } from "react";
+import { Button, Card, Icon, Tooltip } from "@blueprintjs/core";
+import { displayCharClass } from "@/utils/stringUtil";
+import { CharInfoWithStill } from "@/utils/yamlUtil";
 import { getRateEmoji } from "@/utils/rateEnum";
 import RateEmojiSelect from "../RateEmojiSelect";
 
@@ -152,109 +153,120 @@ const StillCard: React.FC<StillCardProps> = (props) => {
   return (
     <Card
       css={css`
-        display: flex;
-        align-items: flex-start;
         width: 100%;
         padding: 15px;
-        gap: 0 15px;
       `}
     >
-      <div
-        css={css`
-          display: flex;
-          flex-direction: column;
-          align-items: center;
-          gap: 5px;
-          position: sticky;
-          top: 10px;
-        `}
-      >
-        <div>{props.children}</div>
-        {props.gridMode && (
-          <Button onClick={props.onClose} small>
-            {t("ui.button.close")}
-          </Button>
-        )}
-      </div>
-      <div
-        css={css`
-          display: flex;
-          flex-wrap: wrap;
-          gap: 5px;
-        `}
-      >
-        {props.char.stills.map((x) => (
+      <LazyLoad height={104}>
+        <div
+          css={css`
+            display: flex;
+            align-items: flex-start;
+            gap: 0 15px;
+          `}
+        >
           <div
-            key={x.id}
             css={css`
-              position: relative;
               display: flex;
+              flex-direction: column;
+              align-items: center;
+              gap: 5px;
+              position: sticky;
+              top: 10px;
             `}
           >
-            <img
-              css={css`
-                width: 185px;
-                height: 104px;
-                opacity: ${x.read ? 1 : 0.7};
-              `}
-              loading="lazy"
-              alt={x.label}
-              src={"/static/image/still/" + x.image}
-              onClick={() => handleReadChange(x.id)}
-            />
-            <Button
-              css={css`
-                position: absolute;
-                top: 50%;
-                left: 50%;
-                transform: translate(-50%, -50%);
-                margin: auto;
-              `}
-              minimal
-              onClick={() => handleReadChange(x.id)}
-            >
-              <Icon size={60} color={x.read ? "#14d13e" : "gray"} icon="tick" />
-            </Button>
-            <div
-              css={css`
-                position: absolute;
-                bottom: 0;
-                right: 0;
-                opacity: 0.85;
-              `}
-            >
-              <RateEmojiSelect
-                value={x.rate.toString()}
-                onChange={(v) => props.onRateChange(x.id, Number(v))}
-                options={[
-                  { value: "-1", label: "💭" },
-                  { value: "0", label: getRateEmoji(0) },
-                  { value: "1", label: getRateEmoji(1) },
-                  { value: "2", label: getRateEmoji(2) },
-                  { value: "3", label: getRateEmoji(3) },
-                  { value: "4", label: getRateEmoji(4) },
-                  { value: "5", label: getRateEmoji(5) },
-                  { value: "6", label: getRateEmoji(6) },
-                ]}
-              />
-            </div>
-            <div
-              css={css`
-                position: absolute;
-                top: 0;
-                left: 0;
-                margin-left: 2px;
-                text-shadow: 1px 1px 0 #000, -1px 1px 0 #000, 1px -1px 0 #000,
-                  -1px -1px 0 #000;
-                text-align: center;
-                font-size: 95%;
-              `}
-            >
-              {x.label}
-            </div>
+            <div>{props.children}</div>
+            {props.gridMode && (
+              <Button onClick={props.onClose} small>
+                {t("ui.button.close")}
+              </Button>
+            )}
           </div>
-        ))}
-      </div>
+          <div
+            css={css`
+              display: flex;
+              flex-wrap: wrap;
+              gap: 5px;
+            `}
+          >
+            {props.char.stills.map((x) => (
+              <div
+                key={x.id}
+                css={css`
+                  position: relative;
+                  display: flex;
+                `}
+              >
+                <img
+                  css={css`
+                    width: 185px;
+                    height: 104px;
+                    opacity: ${x.read ? 1 : 0.7};
+                  `}
+                  loading="lazy"
+                  alt={x.label}
+                  src={"/static/image/still/" + x.image}
+                  onClick={() => handleReadChange(x.id)}
+                />
+                <Button
+                  css={css`
+                    position: absolute;
+                    top: 50%;
+                    left: 50%;
+                    transform: translate(-50%, -50%);
+                    margin: auto;
+                  `}
+                  minimal
+                  onClick={() => handleReadChange(x.id)}
+                >
+                  <Icon
+                    size={60}
+                    color={x.read ? "#14d13e" : "gray"}
+                    icon="tick"
+                  />
+                </Button>
+                <div
+                  css={css`
+                    position: absolute;
+                    bottom: 0;
+                    right: 0;
+                    opacity: 0.85;
+                  `}
+                >
+                  <RateEmojiSelect
+                    value={x.rate.toString()}
+                    onChange={(v) => props.onRateChange(x.id, Number(v))}
+                    options={[
+                      { value: "-1", label: "💭" },
+                      { value: "0", label: getRateEmoji(0) },
+                      { value: "1", label: getRateEmoji(1) },
+                      { value: "2", label: getRateEmoji(2) },
+                      { value: "3", label: getRateEmoji(3) },
+                      { value: "4", label: getRateEmoji(4) },
+                      { value: "5", label: getRateEmoji(5) },
+                      { value: "6", label: getRateEmoji(6) },
+                    ]}
+                  />
+                </div>
+                <div
+                  css={css`
+                    position: absolute;
+                    top: 0;
+                    left: 0;
+                    margin-left: 2px;
+                    text-shadow: 1px 1px 0 #000, -1px 1px 0 #000,
+                      1px -1px 0 #000, -1px -1px 0 #000;
+                    text-align: center;
+                    font-size: 95%;
+                  `}
+                >
+                  {x.label}
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </LazyLoad>
     </Card>
   );
 };
