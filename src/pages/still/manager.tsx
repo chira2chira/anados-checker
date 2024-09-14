59c926371c0e01412a62a17bfa63503ac4785d55
chira <chira2chira2chira2chira@gmail.com>
2024-09-14 13:58:07 +0900

    微修正

--- src/pages/still/manager.tsx
+++ src/pages/still/manager.tsx
@@ -1,6 +1,7 @@
 import { GetStaticProps, NextPage } from "next";
 import { useTranslation } from "next-i18next";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+import { useEffect, useMemo, useState } from "react";
 import { css } from "@emotion/react";
 import { Button, ButtonGroup, ButtonProps, Checkbox } from "@blueprintjs/core";
 import { forceCheck } from "react-lazyload";
@@ -16,7 +17,6 @@ import FilterSelect from "@/components/FilterSelect";
 import ClassButton from "@/components/ClassButton";
 import { sendEvent } from "@/utils/gtag";
 import { TopToaster } from "@/utils/toast";
-import { useEffect, useMemo, useState } from "react";
 import {
   parseLocalStorageChar,
   parseLocalStorageStill,
