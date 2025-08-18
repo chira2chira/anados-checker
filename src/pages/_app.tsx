e6e49f29ad46a58a38a1e317f9d5aa747b8e5519
chira <chira2chira2chira2chira@gmail.com>
2025-08-18 19:18:43 +0900

    why-did-you-render導入

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,7 +3,7 @@ import "@blueprintjs/icons/lib/css/blueprint-icons.css";
 import "@blueprintjs/core/lib/css/blueprint.css";
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
-import { useRouter } from "next/router";
+import React from "react";
 import { appWithTranslation } from "next-i18next";
 import { FocusStyleManager } from "@blueprintjs/core";
 import dayjs from "dayjs";
@@ -12,6 +12,16 @@ import timezone from "dayjs/plugin/timezone";
 import GoogleAnalytics from "../components/GoogleAnalytics";
 import HideSpoilerProvider from "@/providers/HideSpoilerProvider";
 import CustomLabelProvider from "@/providers/CustomLabelProvider";
+
+if (process.env.NODE_ENV === "development") {
+  if (typeof window !== "undefined") {
+    const whyDidYouRender = require("@welldone-software/why-did-you-render");
+    whyDidYouRender(React, {
+      trackAllPureComponents: true,
+      exclude: [/^Blueprint5\./],
+    });
+  }
+}
 
 FocusStyleManager.onlyShowFocusOnTabs();
 
