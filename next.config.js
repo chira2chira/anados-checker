1dd02001ab724ae413b76d27dde5964b24cfb151
chira <chira2chira2chira2chira@gmail.com>
2026-07-28 20:41:59 +0900

    ブラウザキャッシュ対象追加

--- next.config.js
+++ next.config.js
@@ -33,6 +33,24 @@ const nextConfig = {
           },
         ],
       },
+      {
+        source: "/_next/data/:path*",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=31536000, immutable",
+          },
+        ],
+      },
+      {
+        source: "/favicon:path*",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=604800, immutable", // 1 week
+          },
+        ],
+      },
     ];
   },
 };
